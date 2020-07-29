3eb69f8fefbb8e5af21624c7c98b1e5c42a8642e
unknown <samflynn90@gmail.com>
2020-07-29 11:07:15 -0400

    format

--- dist/js/navbar.js
+++ dist/js/navbar.js
@@ -2,18 +2,19 @@
     $(document).ready(function(){
   
       // hide .navbar first
-      $(".navbar-fade").hide();
+    //   $(".navbar-fade").hide();
   
       // fade in .navbar
       $(function () {
           $(window).scroll(function () {
   
                    // set distance user needs to scroll before we start fadeIn
-              if ($(this).scrollTop() > 100) {
+              if ($(this).scrollTop() > 400) {
                   $('.navbar-fade').fadeIn();
               } else {
                   $('.navbar-fade').fadeOut();
-              }
+
+            }
           });
       });
   
